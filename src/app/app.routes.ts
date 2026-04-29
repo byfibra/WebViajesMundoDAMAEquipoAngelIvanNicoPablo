e0fad2e7118f3dab6ae3fdc02692bfc9a109a509
byfibra <angel.lp.318@gmail.com>
2026-04-29 13:52:02 +0200

    Web de Viajes Mundo terminada con Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio';
+import { DestinosComponent } from './destinos/destinos';
+import { ContactoComponent } from './contacto/contacto';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'destinos', component: DestinosComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' }
+];
---
+++ src/app/destinos/destinos.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destinos',
+  standalone: true,
+  imports: [],
+  templateUrl: './destinos.html',
+  styleUrl: './destinos.css'
+})
+export class DestinosComponent {}
