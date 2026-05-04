a37610693ff8b70e3e22e2323b0c3621e468666c
byfibra <angel.lp.318@gmail.com>
2026-05-04 10:42:34 +0200

    configuración app.ts y main.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router'; // Importamos las herramientas
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink], // ¡ESTA LÍNEA ES LA CLAVE!
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: '../styles.css'
 })
 export class AppComponent {
-  title = 'Viajes Mundo';
+  title = 'viajes-mundo';
 }
