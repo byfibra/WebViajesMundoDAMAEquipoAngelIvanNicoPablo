498224e71db1d309bc11514d2acb9de29d8b3755
ihr-vlc <ivan.herranz27@gmail.com>
2026-05-05 21:36:05 +0200

    inicio de sesion y registrarse

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
+  host: { '[class.login]': 'isAuthPage', 'class': 'pt-5 pb-3' }
 })
-export class Footer {}
+export class Footer {
+  isAuthPage = false;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      // Remover el margen del footer en login y register
+      const authRoutes = ['/login', '/register'];
+      this.isAuthPage = authRoutes.includes(event.url);
+    });
+  }
+}
